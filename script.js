c9c4e72bdf6ee6171ef0075d9620933d39e8638e
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-04-14 15:01:33 +0200

    added responsive-design and website is finished

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let dbURL = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
+let dbURL1 = 'https://pokeapi.co/api/v2/pokemon?limit=';
+let limit = 50;
+let dbURL2 = '&offset=0';
+let allPokemonData = null;
+
+
 
 function loadBody() {
     getData();
 }
 
 async function getData() {
-    let response = await fetch(dbURL + '.json');
+    let response = await fetch(getDbURL() + '.json');
     let data = await response.json();
-    showTemplate(data);
+    allPokemonData = data;
+    showTemplate(data)
 }
 
 async function showTemplate(data) {
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = '';
 
-    for (let i = 0; i < data.results.length; i++) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    let loadingRef = document.getElementById('preloader');
+    loadingRef.classList.remove('hidden');
 
+    for (let i = 0; i < data.results.length; i++) {
         let pokemonResponse = await fetch(data.results[i].url);
         let pokemonData = await pokemonResponse.json();
         contentRef.innerHTML += mainTemplate(pokemonData, i);
@@ -24,7 +33,6 @@ async function showTemplate(data) {
         typeRef.innerHTML = '';
 
         for (let j = 0; j < pokemonData.types.length; j++) {
-
             let typeResponse = await fetch(pokemonData.types[j].type.url);
             let typeData = await typeResponse.json();
 
@@ -33,10 +41,13 @@ async function showTemplate(data) {
             typeRef.innerHTML += `<img src='${typeData.sprites['generation-vii']['lets-go-pikachu-lets-go-eevee'].name_icon}' class='type-img'>`;
         }
     }
+
+    loadingRef.classList.add('hidden');
+
 }
 
 async function showCard(i) {
-    let response = await fetch(dbURL + '.json');
+    let response = await fetch(getDbURL() + '.json');
     let data = await response.json();
     let pokemonResponse = await fetch(data.results[i].url);
     let pokemonData = await pokemonResponse.json();
@@ -48,10 +59,8 @@ async function showCard(i) {
     typeRef.innerHTML = '';
 
     for (let j = 0; j < pokemonData.types.length; j++) {
-
         let typeResponse = await fetch(pokemonData.types[j].type.url);
         let typeData = await typeResponse.json();
-        // console.log(typeData);
 
         let value = pokemonData.types[0].type.name;
         switchFuncPopup(value, i);
@@ -61,6 +70,15 @@ async function showCard(i) {
 
     popupRef.classList.remove('hidden');
     document.getElementById('content').classList.add('bright');
+
+    let popupInfoRef = document.getElementById(`popup-info-${i}`);
+    popupInfoRef.innerHTML = '';
+    popupInfoRef.innerHTML = mainInfoTemplate(pokemonData);
+    document.getElementById('main-info').classList.add('border-bottom');
+    let abilitiesRef = document.getElementById('abilities');
+    for (let j = 0; j < pokemonData.abilities.length; j++) {
+        abilitiesRef.innerHTML += `<li>${pokemonData.abilities[j].ability.name}</li>`;
+    }
 }
 
 function closePopup() {
@@ -69,8 +87,7 @@ function closePopup() {
 }
 
 async function showStats(whichStat, i) {
-
-    let response = await fetch(dbURL + '.json');
+    let response = await fetch(getDbURL() + '.json');
     let data = await response.json();
     let pokemonResponse = await fetch(data.results[i].url);
     let pokemonData = await pokemonResponse.json();
@@ -81,24 +98,74 @@ async function showStats(whichStat, i) {
 
     let popupInfoRef = document.getElementById(`popup-info-${i}`);
     popupInfoRef.innerHTML = '';
-    
+
     if (whichStat == 'main') {
         popupInfoRef.innerHTML = mainInfoTemplate(pokemonData);
-
+        document.getElementById('main-info').classList.add('border-bottom');
+        document.getElementById('stats-info').classList.remove('border-bottom');
+        document.getElementById('evo-info').classList.remove('border-bottom');
         let abilitiesRef = document.getElementById('abilities');
-
         for (let j = 0; j < pokemonData.abilities.length; j++) {
             abilitiesRef.innerHTML += `<li>${pokemonData.abilities[j].ability.name}</li>`;
         }
-
     } else if (whichStat == 'stats') {
-
+        document.getElementById('main-info').classList.remove('border-bottom');
+        document.getElementById('stats-info').classList.add('border-bottom');
+        document.getElementById('evo-info').classList.remove('border-bottom');
         popupInfoRef.innerHTML = statsTemplate(pokemonData);
-
     } else if (whichStat == 'evo') {
-        popupInfoRef.innerHTML = evoTemmplate();
+        document.getElementById('main-info').classList.remove('border-bottom');
+        document.getElementById('stats-info').classList.remove('border-bottom');
+        document.getElementById('evo-info').classList.add('border-bottom');
+        promiseEvo(evoChainData, popupInfoRef);
 
-        promiseEvo(evoChainData);
+
     }
 }
 
+async function search() {
+
+    let searchInput = document.getElementById('searchbar').value.toLowerCase();
+    if (document.getElementById('searchbar').value.length >= 2) {
+        document.getElementById('search-p').classList.add('hidden');
+        let response = await fetch(getDbURL());
+        let data = await response.json();
+        let contentRef = document.getElementById('content');
+        contentRef.innerHTML = '';
+
+        for (let i = 0; i < data.results.length; i++) {
+            let name = data.results[i].name.toLowerCase();
+
+            if (name.includes(searchInput)) {
+                let pokemonResponse = await fetch(data.results[i].url);
+                let pokemonData = await pokemonResponse.json();
+
+                contentRef.innerHTML += mainTemplate(pokemonData, i);
+
+                let typeRef = document.getElementById(`type-${i}`);
+                typeRef.innerHTML = '';
+
+                for (let j = 0; j < pokemonData.types.length; j++) {
+                    let typeResponse = await fetch(pokemonData.types[j].type.url);
+                    let typeData = await typeResponse.json();
+
+                    let value = pokemonData.types[0].type.name;
+                    switchFunc(value, i);
+                    typeRef.innerHTML += `<img src='${typeData.sprites['generation-vii']['lets-go-pikachu-lets-go-eevee'].name_icon}' class='type-img'>`;
+                }
+            }
+        }
+    } else {
+        document.getElementById('search-p').classList.remove('hidden');
+        showTemplate(allPokemonData);
+    }
+}
+
+function loadMoreContent() {
+    limit += 50;
+    getData();
+}
+
+function getDbURL() {
+    return dbURL1 + `${limit}` + dbURL2;
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,5 @@
 function mainTemplate(pokemonData, i) {
-   return `<div class='card' onclick='showCard(${i})'>
+    return `<div class='card' onclick='showCard(${i})'>
                 <div class='card-header'>
                     <h2>#${pokemonData.id}</h2>
                     <h2>${pokemonData.species.name}</h2>
@@ -13,7 +13,7 @@ function mainTemplate(pokemonData, i) {
 
 function popupTemplate(pokemonData, i) {
     return `<div class='popup-card'>
-                                <div class='card-header'>
+                                <div class='card-header card-header-1000px'>
                                     <h2>#${pokemonData.id}</h2>
                                     <h2>${pokemonData.species.name}</h2>
                                     <button onclick='closePopup()'>X</button>
@@ -22,9 +22,9 @@ function popupTemplate(pokemonData, i) {
                                 <div class='popup-card-type' id='popup-type-${i}'>
                                 </div>
                                 <div class='popup-nav'>
-                                    <div class='popup-nav-element' onclick='showStats("main",${i})'>main</div>
-                                    <div class='popup-nav-element' onclick='showStats("stats",${i})'>stats</div>                                        
-                                    <div class='popup-nav-element' onclick='showStats("evo",${i})'>evo</div>
+                                    <div class='popup-nav-element' onclick='showStats("main",${i})' id='main-info'>main</div>
+                                    <div class='popup-nav-element' onclick='showStats("stats",${i})' id='stats-info'>stats</div>                                        
+                                    <div class='popup-nav-element' onclick='showStats("evo",${i})' id='evo-info'>evo</div>
                                 </div>
                                 <div class='popup-info' id='popup-info-${i}'>
                                 </div>
@@ -55,7 +55,7 @@ function mainInfoTemplate(pokemonData) {
 }
 
 function statsTemplate(pokemonData) {
-    return`<div class='bars-div'>
+    return `<div class='bars-div'>
             HP
             <div class="progress-bar">
                 <div class="progress-fill" style="width: ${pokemonData.stats[0].base_stat}%;">
@@ -91,22 +91,39 @@ function statsTemplate(pokemonData) {
         </div>`;
 }
 
-function evoTemmplate() {
-    return `<div class='evo-chain'>
-                <div class='evo-chain-img' id='first-evo'></div>  
-                     <div class='svg'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="25" height="25">
-                         <polygon points="5,10 95,50 5,90" fill="white">
-                        <animate attributeName="opacity" from="0" to="1" dur="1s" begin="0s" />
-                        <animate attributeName="transform" type="scale" from="0" to="1" dur="1s" begin="0s" />
-                         </polygon>
-                     </svg></div>
-                 <div class='evo-chain-img' id='second-evo'></div>
-                     <div class='svg'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="25" height="25">
-                         <polygon points="5,10 95,50 5,90" fill="white">
-                         <animate attributeName="opacity" from="0" to="1" dur="1s" begin="0s" />
-                         <animate attributeName="transform" type="scale" from="0" to="1" dur="1s" begin="0s" />
-                         </polygon>
-                     </svg></div>
-                 <div class='evo-chain-img' id='third-evo'></div>
-             </div>`
+function evoTemmplate(evoValue) {
+    if (evoValue == 'one') {
+        return `<div class='evo-chain'>
+                    <div class='evo-chain-img' id='first-evo'></div> 
+                </div>`
+    } else if (evoValue == 'two') {
+        return `<div class='evo-chain'>
+                    <div class='evo-chain-img' id='first-evo'></div>  
+                        <div class='svg'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="25" height="25">
+                            <polygon points="5,10 95,50 5,90" fill="white">
+                            <animate attributeName="opacity" from="0" to="1" dur="1s" begin="0s" />
+                            <animate attributeName="transform" type="scale" from="0" to="1" dur="1s" begin="0s" />
+                            </polygon>
+                        </svg></div>
+                    <div class='evo-chain-img' id='second-evo'></div>
+                </div>`
+    } else {
+        return `<div class='evo-chain'>
+                    <div class='evo-chain-img' id='first-evo'></div>  
+                        <div class='svg'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="25" height="25">
+                            <polygon points="5,10 95,50 5,90" fill="white">
+                            <animate attributeName="opacity" from="0" to="1" dur="1s" begin="0s" />
+                            <animate attributeName="transform" type="scale" from="0" to="1" dur="1s" begin="0s" />
+                            </polygon>
+                        </svg></div>
+                    <div class='evo-chain-img' id='second-evo'></div>
+                        <div class='svg' id='svg'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="25" height="25">
+                            <polygon points="5,10 95,50 5,90" fill="white">
+                            <animate attributeName="opacity" from="0" to="1" dur="1s" begin="0s" />
+                            <animate attributeName="transform" type="scale" from="0" to="1" dur="1s" begin="0s" />
+                            </polygon>
+                        </svg></div>
+                    <div class='evo-chain-img' id='third-evo'></div>
+                </div>`
+    }
 }
